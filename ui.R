bca5f10917fc5590736ed319f35fc5e793d0aa48
Teo Wei Shen <tws4793@gmail.com>
2019-11-21 03:46:04 +0800

    Revised all code

--- ui.R
+++ ui.R
@@ -1,213 +1,283 @@
-# install.packages('shinydashboard')
-library(shinydashboard)
-
+# UI
 common = fluidRow(
+  column(
+    width = 12,
+    column(width = 1),
+    valueBoxOutput('over_n_country', width = 2),
+    valueBoxOutput('over_region_study', width = 2),
+    valueBoxOutput('over_region_score', width = 2),
+    valueBoxOutput('over_oecd_study', width = 2),
+    valueBoxOutput('over_oecd_score', width = 2),
+    column(width = 1)
+  )
+)
+
+p1 = fluidRow( # Overview of Time Spent Studying and Score
+  common,
+  fluidRow(
     column(
-        width = 12,
-        box(
-            title = 'No of Countries',
-            width = 4,
-            textOutput('over_n_country')
+      width = 12,
+      box(
+        title = 'Settings',
+        status = 'primary',
+        width = 2,
+        solidHeader = T,
+        radioButtons(
+          inputId = 'in_rb_result_main',
+          label = 'Select',
+          choices = c('Study Time', 'Score'),
+          selected = 'Study Time'
         ),
-        box(
-            title = 'Singapore Mean',
-            width = 4,
-            textOutput('over_sg')
+        hr(),
+        radioButtons(
+          inputId = 'in_rb_region_main',
+          label = 'Region',
+          choices = c(
+            'APAC',
+            'EMER',
+            'AMERICA'
+          )
+        )
+      ),
+      box(
+        title = 'Country View',
+        status = 'primary',
+        width = 5,
+        solidHeader = T,
+        leafletOutput('plot_map')
+      ),
+      box(
+        title = 'Top 10 Countries',
+        status = 'primary',
+        width = 5,
+        solidHeader = T,
+        plotOutput('plot_bc_top_cnt')
+      )
+    )
+  )
+)
+
+p2 = fluidRow( # Time Spent Studying vs Score
+  fluidRow(
+    column(
+      width = 12,
+      box(
+        title = 'Settings',
+        status = 'primary',
+        width = 2,
+        solidHeader = T,
+        radioButtons(
+          inputId = 'in_rb_result_ts',
+          label = 'Study Time',
+          choices = c('Total', 'Outside School'),
+          selected = 'Total'
+        )
+        # selectInput(
+        #   inputId = 'in_dd_country_sc',
+        #   label = 'Select Country',
+        #   choices = NULL
+        # )
+      ),
+      box(
+        title = 'Does spending more time studying results in better scores?',
+        status = 'primary',
+        width = 10,
+        solidHeader = T,
+        # h2('Does spending more time studying results in better scores?'),
+        plotOutput('plot_sc_score_stime')
+      )
+    )
+  )
+)
+
+p3 = fluidRow( # Compare Gender
+  fluidRow(
+    column(
+      width = 12,
+      column(width = 2),
+      valueBoxOutput('over_m_sg', width = 2),
+      valueBoxOutput('over_m_all', width = 2),
+      valueBoxOutput('over_f_sg', width = 2),
+      valueBoxOutput('over_f_all', width = 2),
+      column(width = 2)
+    )
+  ),
+  fluidRow(
+    column(
+      width = 12,
+      box(
+        title = 'Settings',
+        status = 'primary',
+        width = 2,
+        solidHeader = T,
+        radioButtons(
+          inputId = 'in_rb_sg_gen',
+          label = 'Select',
+          choices = c('Study Time', 'Score')
+        )
+      ),
+      box(
+        title = 'How do Males compare to Females...',
+        status = 'primary',
+        width = 10,
+        solidHeader = T,
+        plotOutput('plot_bp_gen')
+      )
+    )
+  )
+)
+
+p4 = fluidRow( # Time Spent Studying vs Score - Boxplot
+  fluidRow(
+    column(
+      width = 12,
+      box(
+        title = 'Settings',
+        status = 'primary',
+        width = 2,
+        solidHeader = T,
+        radioButtons(
+          inputId = 'in_rb_sg_factor',
+          label = 'Select',
+          choices = c('Study Time', 'Score')
         ),
-        box(
-            title = 'OECD Mean',
-            width = 4,
-            textOutput('over_oecd')
+        selectInput(
+          inputId = 'in_dd_stu_factor',
+          label = 'Select Comparison Factors',
+          choices = NULL
         )
+      ),
+      box(
+        title = textOutput('txt_factor_graph'),
+        status = 'primary',
+        width = 10,
+        solidHeader = T,
+        plotOutput('plot_bp_material')
+      )
     )
+  )
 )
 
-p1 = fluidRow(
-    common,
-    fluidRow(
-        column(
-            width = 12,
-            box(
-                title = 'Settings',
-                status = 'primary',
-                width = 3,
-                solidHeader = T,
-                radioButtons(
-                    inputId = 'in_rb_result_main',
-                    label = 'Select',
-                    choices = c('Study Time', 'Score'),
-                    selected = 'Study Time'
-                ),
-                hr(),
-                radioButtons(
-                    inputId = 'in_rb_region_main',
-                    label = 'Region',
-                    choices = c(
-                        'ALL',
-                        'APAC',
-                        'EMEA',
-                        'Americas'
-                    )
-                )
-            ),
-            box(
-                title = 'Country View',
-                status = 'primary',
-                width = 5,
-                solidHeader = T,
-                #plotOutput(),
-                br(),
-                # plotOutput('plot_bw_cnt', click = 'cl_bw_cnt')
-            ),
-            box(
-                title = 'Top 10 Countries',
-                status = 'primary',
-                width = 4,
-                solidHeader = T,
-                # plotOutput('bar_top_cnt', click = 'cl_bar_top_cnt')
-            )
-        )
-    )
-)
-
-p2 = fluidRow(
-    common,
-    fluidRow(
-        column(
-            width = 12,
-            box(
-                title = 'Settings',
-                status = 'primary',
-                width = 3,
-                solidHeader = T,
-                selectInput(
-                    inputId = 'in_dd_country_sc',
-                    label = 'Select Country',
-                    choices = NULL
-                )
-            ),
-            box(
-                title = 'Scatter Plot',
-                status = 'primary',
-                width = 9,
-                solidHeader = T,
-                # plotOutput('scatter_compare_cnt', click = 'scatter_cnt')
-            )
-        )
-    )
-)
-
-p3 = fluidRow(
-    common,
-    fluidRow(
-        column(
-            width = 12,
-            box(
-                title = 'Settings',
-                status = 'primary',
-                width = 3,
-                solidHeader = T,
-                selectInput(
-                    inputId = 'in_dd_country_bp',
-                    label = 'Select Country',
-                    choices = NULL
-                )
-            ),
-            box(
-                title = 'Result',
-                status = 'primary',
-                width = 9,
-                solidHeader = T,
-                # plotOutput('scatter_compare_cnt', click = 'scatter_cnt')
-            )
-        )
-    )
-)
-
-p4 = fluidRow(
-    fluidRow(
-
+p5 = fluidRow( # Predict Score by Material Factors
+  column(
+    width = 12,
+    box(
+      title = 'Settings',
+      status = 'primary',
+      width = 2,
+      solidHeader = T,
+      radioButtons(
+        inputId = 'in_rb_stu_factor1',
+        label = 'Gender',
+        inline= TRUE,
+        choices = c('Male', 'Female')
+      ),
+      checkboxInput(
+        inputId = 'in_cb_stu_factor2',
+        label = 'Repeated Grade',
+        value = F
+      ),
+      checkboxInput(
+        inputId = 'in_cb_stu_factor3',
+        label = 'Native Parents',
+        value = T
+      ),
+      checkboxInput(
+        inputId = 'in_cb_stu_factor4',
+        label = 'Owns Computer',
+        value = T
+      ),
+      numericInput(
+        inputId = 'in_tb_stu_factor5',
+        label = 'Number of Cars',
+        value = 0,
+        min = 0,
+        max = 5
+      ),
+      numericInput(
+        inputId = 'in_tb_stu_factor6',
+        label = 'Number of Books',
+        value = '11',
+        min = 0
+      ),
+      selectInput(
+        inputId = 'in_sc_stu_factor7',
+        label = 'Parents care for me',
+        choices = opt_ag_scale
+      ),
+      selectInput(
+        inputId = 'in_sc_stu_factor8',
+        label = 'I am ambitious',
+        choices = opt_ag_scale
+      ),
+      selectInput(
+        inputId = 'in_sc_stu_factor9',
+        label = 'I worry that a test would be difficult',
+        choices = opt_ag_scale
+      ),
+      selectInput(
+        inputId = 'in_sc_stu_factor10',
+        label = 'I feel anxious even if I am prepared',
+        choices = opt_ag_scale
+      ),
     ),
-    fluidRow(
-        column(
-            width = 12,
-            box(
-                title = 'Scatter',
-                status = 'primary',
-                width = 3,
-                solidHeader = T,
-                radioButtons(
-                    inputId = 'in_rb_result_sc',
-                    label = 'Select',
-                    choices = c('Study Time', 'Score')
-                ),
-                checkboxGroupInput(
-                    inputId = 'in_cb_posessions_sc',
-                    label = 'Select Some Posessions',
-                    choices = NULL
-                )
-            ),
-            box(
-                title = 'Result',
-                status = 'primary',
-                width = 9,
-                solidHeader = T
-            )
-        )
+    box(
+      title = 'Final Predicted Score',
+      status = 'primary',
+      width = 10,
+      solidHeader = T,
+      span(
+        strong(textOutput('txt_final_predict_result')),
+        style='font-size: 200px',
+        align='center'
+      )
     )
-)
-
-p5 = fluidRow(
-    column(
-        width = 12,
-        box(
-            title = 'Settings',
-            status = 'primary',
-            width = 3,
-            solidHeader = T,
-            checkboxGroupInput(
-                inputId = 'in_cb_posessions_fsc',
-                label = 'Select Some Posessions',
-                choices = NULL
-            )
-        ),
-        box(
-            title = 'Final Predicted Score',
-            status = 'primary',
-            width = 9,
-            solidHeader = T,
-            textOutput('txt_final_predict_result')
-        )
-    )
+  )
 )
 
 header = dashboardHeader(
-    title = 'Education'
+  title = textOutput('title')
+  # dropdownMenu(
+  #   type = 'messages',
+  #   messageItem(
+  #     from = 'G3 T2',
+  #     message = 'Hello!',
+  #     time = '11:30'
+  #   )
+  # )
 )
 
-sidebar = dashboardSidebar(
-    sidebarMenu(
-        menuItem('Time Spent Studying', tabName = 'main', icon = icon('dashboard')),
-        menuItem('Measuring Countries', tabName = 'two'),
-        menuItem('Result - World', tabName = 'three'),
-        menuItem('Result - Singapore', tabName = 'four'),
-        menuItem('Prediction', tabName = 'five')
+sidebar = dashboardSidebar( #change to blue
+  sidebarMenu(
+    menuItem('Global', startExpanded = T,
+      menuSubItem('Overview', tabName = 'index', icon = icon('dashboard')),
+      menuSubItem('Time vs Score', tabName = 'time-score', icon = icon('line-chart'))
+    ),
+    menuItem('Singapore', startExpanded = T,
+      menuSubItem('Overview', tabName = 'singapore', icon = icon('dashboard')),
+      menuSubItem('Analysis', tabName = 'analysis', icon = icon('globe')),
+      menuSubItem('Prediction', tabName = 'prediction', icon = icon('line-chart'))
+    ),
+    menuItem('About', startExpanded = T,
+      menuSubItem('About', tabName = 'about', icon = icon('info'))
     )
+  )
 )
 
 body = dashboardBody(
-    tabItems(
-        tabItem(tabName = 'main', p1),
-        tabItem(tabName = 'two', p2),
-        tabItem(tabName = 'three', p3),
-        tabItem(tabName = 'four', p4),
-        tabItem(tabName = 'five', p5)
-    )
+  tabItems(
+    tabItem(tabName = 'index', p1),
+    tabItem(tabName = 'time-score', p2),
+    tabItem(tabName = 'singapore', p3),
+    tabItem(tabName = 'analysis', p4),
+    tabItem(tabName = 'prediction', p5),
+    tabItem(tabName = 'about', includeMarkdown('about.md'))
+  )
 )
 
 ui = dashboardPage(
-# skin = 'black',
-    header,
-    sidebar,
-    body
+  skin = 'red',
+  header,
+  sidebar,
+  body
 )
